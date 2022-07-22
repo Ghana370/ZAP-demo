481e4b2b9647f8cb24045597eae38be268934498
Ghana370 <87018143+Ghana370@users.noreply.github.com>
2022-07-22 17:55:39 +0530

    Update zap-demo.sh

--- zap-demo.sh
+++ zap-demo.sh
@@ -3,8 +3,8 @@
 CONTAINER_ID=$(docker run -u zap -p 2375:2375 -d owasp/zap2docker-weekly zap.sh -daemon -port 2375 -host 127.0.0.1 -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true)
 
 # the target URL for ZAP to scan
-TARGET_URL="http://demo.testfire.net/"
-docker exec $CONTAINER_ID zap-cli chmod u+x /tmp/jenkins5882197837504659969.sh
+TARGET_URL=$target_url
+
 docker exec $CONTAINER_ID zap-cli -p 2375 status -t 120 && docker exec $CONTAINER_ID zap-cli -p 2375 open-url $TARGET_URL
 
 docker exec $CONTAINER_ID zap-cli -p 2375 spider $TARGET_URL
